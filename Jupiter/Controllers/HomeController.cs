bd2bcc7d03158b71881f10209b5a117e68ae3584
tamifist <v.omelyanchuk@outlook.com>
2014-11-27 19:26:29 +0200

    Jupiter-21: Create Website for App

---
+++ Jupiter/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+
+namespace Jupiter.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Jupiter is available on all your devices";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Jupiter app description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "My contacts page.";
+
+            return View();
+        }
+    }
+}
